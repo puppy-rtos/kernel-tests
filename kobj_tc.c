3a3728da39508429c652effd94327f9fda7abf57
guozhanxin <guozhanxin@rt-thread.com>
2022-11-19 21:28:23 +0800

    add p_list tests

--- kobj_tc.c
+++ kobj_tc.c
@@ -24,3 +24,56 @@ static void kobj_tc(void)
     P_UNUSED(p_obj_t_value);
     P_UNUSED(err);
 }
+
+static struct _klist_tc_t
+{
+    int index;
+    p_node_t node;
+}_tc_list[3];
+
+static void klist_tc(void)
+{
+    p_list_t list = P_LIST_STATIC_INIT(&list);
+    int i;
+    p_node_t *node, *node_s;
+    struct _klist_tc_t *list_tc_ptr;
+
+    if (p_list_is_empty(&list) != true)
+    {
+        P_TC_FAIL();
+    }
+
+    for (i = 0; i < 3; i ++)
+    {
+        _tc_list[i].index = i;
+        p_list_append(&list, &_tc_list[i].node);
+    }
+
+    i = 0;
+    p_list_for_each_node(&list, node)
+    {
+        list_tc_ptr = p_container_of(node, struct _klist_tc_t, node);
+        if (list_tc_ptr->index != i)
+        {
+            P_TC_FAIL();
+            return;
+        }
+        i ++;
+    }
+
+    p_list_for_each_node_safe(&list, node, node_s)
+    {
+        p_list_remove(node);
+    }
+
+    if (p_list_is_empty(&list) == false)
+    {
+        P_TC_FAIL();
+    }
+    P_TC_PASS();
+}
+
+void kobj_tc_main(void)
+{
+    klist_tc();
+}
